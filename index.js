1dadf110e12d87473a9b699bedab0618bf73c10e
Johnny Dickerson Jr <lilman.jdick@gmail.com>
2021-07-02 15:53:56 -0500

    added questions to index js

--- index.js
+++ index.js
@@ -5,7 +5,58 @@ const fs = require('fs');
 const path = require('path');
 
 // TODO: Create an array of questions for user input
-const questions = [];
+const questions = [
+    {
+        type: "input",
+        name: "title",
+        message: "What is the name of your project?",
+    },
+    {
+        type: "input",
+        name: "description",
+        message: "Please describe your project?",
+    },
+    {
+        type: "input",
+        name: "installation",
+        message: "How do you install dependencies from the package.JSON?",
+        default: "npm i"
+    },
+    {
+        type: "input",
+        name: "usage",
+        message: "How do you use this REPO?",
+    },
+    {
+        type: "input",
+        name: "contribution",
+        message: "How do people contribute to this REPO?",
+    },
+    {
+        type: "list",
+        name: "license",
+        message: "Which license do you want to use?",
+        choices: ["MIT", "GPLv2", "Apache", "None"],
+    },
+    {
+        type: "input",
+        name: "username",
+        message: "What is your Github user name?",
+    },
+    {
+        type: "input",
+        name: "email",
+        message: "What is your email address?",
+    },
+    {
+        type: "input",
+        name: "test",
+        message: "How do you run tests?",
+        default: "npm test"
+    },
+
+
+];
 
 // TODO: Create a function to write README file
 function writeToFile(fileName, data) {}
